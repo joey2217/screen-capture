9f39ddc285360e885faa9207e007b730b23d3d96
Joey <786678257@qq.com>
2021-08-17 11:19:34 +0800

    feat✨:  ScreenRecord

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,10 @@
-import { Button, Space } from "antd"
-import { useEffect, useRef } from "react"
-import { getCaptureStream } from '../utils/screenCapture'
+import { useRef, useState } from "react"
+import { Button } from "antd"
+import { getCaptureStream, startRecordCapture, stopRecord } from '../utils/screenCapture'
 
 export default function Home() {
   const videoEl = useRef<HTMLVideoElement | null>(null)
-
-  useEffect(()=>{
-    if (videoEl.current) {
-      videoEl.current.onplay = ()=>{
-        console.log('onplay');
-      }
-      videoEl.current.onpause = ()=>{
-        console.log('onpause');
-      }
-    }
-  },[])
+  const [playing, setPlaying] = useState(false)
 
   const onStartCapture = async () => {
     const stream = await getCaptureStream()
@@ -22,29 +12,44 @@ export default function Home() {
       videoEl.current.srcObject = stream
       videoEl.current.onloadedmetadata = () => {
         videoEl.current!.play()
+        setPlaying(true)
+        startRecordCapture(stream, onStopCapture)
         // @ts-ignore
-        videoEl.current!.requestPictureInPicture().catch(err=>{
+        videoEl.current!.requestPictureInPicture().catch(err => {
           console.error(err);
         })
       }
     }
   }
 
-  const onStopCapture = () => {
-    const tracks = (videoEl.current!.srcObject as MediaStream).getTracks()
-    tracks.forEach(track => track.stop())
-    videoEl.current!.srcObject = null
+  const onStopCapture = (bool?: boolean) => {
+    try {
+      if (bool) {
+        stopRecord()
+      }
+      setPlaying(false)
+      // @ts-ignore
+      document.exitPictureInPicture()
+      // @ts-ignore
+      const tracks = (videoEl.current!.srcObject as MediaStream).getTracks()
+      tracks.forEach(track => track.stop())
+      videoEl.current!.srcObject = null
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   return (
-    <div>
-      <div className="text-center">
+    <div className="text-center">
+      <div>
         <video ref={videoEl} className="max-w-4xl w-full border border-solid border-gray-500"></video>
       </div>
-      <Space>
-        <Button type="primary" onClick={onStartCapture}>Start</Button>
-        <Button type="primary" onClick={onStopCapture}>End</Button>
-      </Space>
+      <div>
+        {
+          playing ? <Button type="primary" onClick={() => onStopCapture(true)}>Stop</Button> :
+            <Button type="primary" onClick={onStartCapture}>Start</Button>
+        }
+      </div>
     </div>
   )
 }
--- utils/screenCapture.ts
+++ utils/screenCapture.ts
@@ -11,3 +11,49 @@ export function getCaptureStream(): Promise<MediaStream> {
     // @ts-ignore
     return navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
 }
+
+let recorder: any;
+let recordedChunks: any[] = []
+let numRecordedChunks = 0
+
+export function startRecordCapture(stream: MediaStream, onRecorderStop: () => void) {
+    recordedChunks = []
+    // @ts-ignore
+    recorder = new MediaRecorder(stream)
+    recorder.ondataavailable = recorderOnDataAvailable
+    recorder.onstop = () => {
+        onRecorderStop()
+        download()
+        console.log('recorderOnStop fired');
+    }
+    recorder.start()
+}
+
+function recorderOnDataAvailable(event: any) {
+    if (event.data && event.data.size > 0) {
+        recordedChunks.push(event.data)
+        numRecordedChunks += event.data.byteLength
+    }
+}
+
+export function stopRecord() {
+    recorder.stop()
+    recorder = null
+    // TODO
+}
+
+const download = () => {
+    let blob = new Blob(recordedChunks, { type: 'video/mp4' })
+    let url = URL.createObjectURL(blob)
+    let a = document.createElement('a')
+    document.body.appendChild(a)
+    // @ts-ignore
+    a.style = 'display: none'
+    a.href = url
+    a.download = `screen-recorder-${new Date().toLocaleString('zh-CN', { hour12: false })}.mp4`
+    a.click()
+    setTimeout(function () {
+        document.body.removeChild(a)
+        window.URL.revokeObjectURL(url)
+    }, 100)
+}
